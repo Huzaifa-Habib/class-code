e80c867f2ef2a90d14aa6a3c528bd3bcfa3161bb
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-06-11 21:46:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+
+
+// Question 1=======================================================
+
+// for (var i = 1; i <= 5; i++) 
+// {
+//    alert("Hello World")
+// }
+
+
+// Question 2========================================================
+
+for (var i = 1; i <=10; i++) {
+    document.write(i + "</br>")
+}
+
+document.write("</br>");
+document.write("</br>");
+
+
+// Question 3===========================================================
+
+var a= prompt("Enter the number for its multiplication table");
+var b= prompt("Enter the length of number for its multiplication table");
+
+for (var i = 1 ; i <= b; i++) {
+    var result = i * a;
+    document.write(a + " X " + i + " = " + result + "</br>")
+
+}
+
+
+// Quetion 4 ========================================================
+
+var q= ["Nokia", "Samsung", "Apple", "Sony", "Huawei"]
+
+for (var i=0; i < q.length; i++) {
+    document.write(q [i] );
+    document.write( "</br>")
+
+}
+
+document.write( "</br>")
+document.write( "</br>")
+
+
+
+// Question 5===================================================
+
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+for (var i=0; i < fruits.length; i++) {
+    document.write(fruits [i] );
+    document.write( "</br>")
+
+}
+
+for (var i = 0; fruits.length; i++) {
+    document.write(fruits[i]);
+
+}
+
+
+
+  
